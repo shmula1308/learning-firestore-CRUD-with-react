890f4adc63c07c125547cdd6516bea48873057d1
Shpend Mula <shmula@hotmail.com>
2022-02-08 15:10:05 +0100

    teste basics of storage and upload functionality

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import FirestoreContext from "../contexts/FirestoreContext";
 import useGetCollectionOnChange from "../../hooks/useGetCollectionOnChange";
 import SignOutBtn from "../SignOutButton/SignOutBtn";
+import { auth } from "../Firebase/firebase";
 import { db } from "../Firebase/firebase";
+import { storage } from "../Firebase/firebase";
+import { ref, uploadBytes, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import {
   onSnapshot,
   collection,
@@ -31,6 +34,7 @@ const HomePage = () => {
   const [lastVisible, setLastVisible] = useState(null);
   const [firstVisible, setFirstVisible] = useState(null);
   const [documentSnapshotSize, setDocumentSnapshotSize] = useState(null);
+  const [progress, setProgress] = useState(0);
 
   //const [users, setUsers] = useState(null);
   const dbCtx = useContext(FirestoreContext);
@@ -186,6 +190,94 @@ const HomePage = () => {
     }
   };
 
+  const onStorageHandler = () => {
+    const storageRef = ref(storage);
+    console.log(storageRef); // ref object for root of storage
+    console.log(storageRef.fullPath); // nothing comes up for root
+    console.log(storageRef.name); // nothing
+    console.log(storageRef.bucket); //learning-firestore-with-react.appspot.com
+
+    const eventLoopRef = ref(storage, "images/eventLoop.png");
+    console.log(eventLoopRef); // Reference object
+    console.log(eventLoopRef.parent); // reference to images object
+    console.log("fullPath", eventLoopRef.parent.fullPath); //images
+    console.log("bucket", eventLoopRef.parent.bucket); //learning-firestore-with-react.appspot.com
+    console.log("name", eventLoopRef.parent.name);
+  };
+  const fileRef = useRef();
+
+  const onSubmitFile = (ev) => {
+    ev.preventDefault();
+    const uid = auth.currentUser.uid;
+    //const file = fileRef.current.value; // dont know how to make ref work. code below works. I think ref only retun the value of ref not the actual file. That's why we need to get the file from ev target files
+    const file = ev.target[0].files[0];
+    console.log(file);
+    if (!file) {
+      return;
+    }
+    // Think about how you will get the basic metadat of the files being uploaded. getMetadata, updateMetadata
+    const storageRef = ref(storage, `files/${uid}`);
+
+    // this is a way to upload a file without showing any progress bar
+    // uploadBytes(storageRef, file).then((snapshot) => {
+    //   console.log("Uploaded a blob or file!", snapshot);
+    // });
+
+    // this is how to show progress bar
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    // uploadtask(observer,snapshot,error,success)
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        console.log(snapshot.bytesTransferred, snapshot.totalBytes);
+        const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+        console.log(progress);
+        setProgress(progress);
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/unauthorized":
+            // User doesn't have permission to access the object
+            break;
+          case "storage/canceled":
+            // User canceled the upload
+            break;
+
+          // ...
+
+          case "storage/unknown":
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
+      }
+    );
+
+    console.log(auth.currentUser);
+  };
+
+  const uploadFile = () => {
+    const file = fileRef.current.value;
+    const storageRef = ref(storage, `files/${file}`);
+
+    // uploadBytes(storageRef, file).then((snapshot) => {
+    //   console.log("Uploaded a blob or file!", snapshot);
+    // });
+    console.log(fileRef.current.name);
+  };
+
   return (
     <div>
       <h1>Homepage</h1>
@@ -197,9 +289,20 @@ const HomePage = () => {
           })}
       </ul>
       <div>
+        <form onSubmit={onSubmitFile}>
+          <div>
+            <input ref={fileRef} type='file'></input>
+          </div>
+          <button type='submit'>Upload File</button>
+        </form>
+        <h3>Uploaded:{progress} % </h3>
+
+        {/* {progress === 0 ? "" : <h3>Uploaded:{progress} %</h3>} */}
+        <hr />
         <button onClick={onPreviousPageHandler}>Previous</button>
         <button onClick={onNextPageHandler}>Next</button>
         <button onClick={onUpdateHandler}>Update City</button>
+        <button onClick={onStorageHandler}>Storage Experiment</button>
       </div>
       <SignOutBtn />
     </div>
